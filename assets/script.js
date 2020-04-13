e2bd334a1af90e7b5c33fa40f09a657f86905938
Bryce Speirs <baspeirs@gmail.com>
2020-04-12 21:43:27 -0400

    the begining of the program, an empty html file, empty css file, and a js file with some instructions to get started

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+// create a start button quiz that does 2 things
+    // 1) starts a timer (you've done this before, go back through previous excersizes)
+    // 2) starts the quiz. we want it to hide the start button, and show the first question
+// every time a question is answered we need to
+    // 1) remove the current question and present another (until done)
+    // 2) keep score (wrong answers reduce time)
+// when questions run out, or time runs out, we need to 
+    // 1) present the score
+    // 2) leave a form to submit initials
+    // 3) save the form to localStorage so it can be accessed later
+   
+// questions:
+    //  commonly used data types do not include
+    // the condition of an if else statement is enclosed within..
+    // arrays in javascript can be used to store..
+    // string values must be enclosed in ___ when being used to assign varriables
+    // a very useful too to be used during development and debugging and printing content to the terminal is...   
