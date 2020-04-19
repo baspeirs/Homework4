a631fe9adc2875387f5649e5c7a3f82d0b6150ba
Bryce Speirs <baspeirs@gmail.com>
2020-04-18 20:31:02 -0400

    finished the logic, added a last score to the front page

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,51 @@
-var parentList = document.querySelector('#questionSection')
 var startQuiz = document.querySelector('#startBtn');
+var scoreList = document.querySelector('#highScores')
+var frontContent = document.getElementById('initialContent');
 var time = document.querySelector('#counter');
 var questionList = document.querySelector("#questionsBox");
 var secondsLeft = 90; 
 var questionIndex = 0;
 var questionText = document.getElementById('question');
 var answerText = document.querySelectorAll('.btn')
+var scorePage = document.querySelector('#finalContent')
+var initialsInput = document.querySelector('#initialsInput');
+var submittedScore = document.querySelector('#submitScore');
+var finalScore = document.querySelector('#finalScore');
+var highScoreSet = {
+    initials: highScoreSet,
+    score: submittedScore
+}
+var lastUser = localStorage.getItem("highscore");
+lastUser = JSON.parse(lastUser)
+console.log(lastUser)
+// create a function to be called when the page loads to generate a list of the high scores
+function scoreListGenerate() {
+    let listItem = document.createElement('li')
+    listItem.textContent = "Initials: " + lastUser.initials + " score: " + lastUser.score;
+    scoreList.appendChild(listItem)
+    
+}
+scoreListGenerate();
+//frontContent.appendChild(lastUser)
+function endQuiz () {
+    scorePage.classList.remove('hide');
+    questionList.classList.add('hide');
+    time.classList.add('hide');
+    finalScore.textContent = score;
+    submittedScore.addEventListener('click', function() {
+        var highScoreSet = {
+            initials: initialsInput.value,
+            score: submittedScore.value
+        }
+        highScoreSet.initials = initialsInput.value
+        highScoreSet.score = score
+        let userScore = JSON.stringify(highScoreSet)
+        localStorage.setItem('highscore', userScore)
+        
+        console.log(highScoreSet)
+    })
+    
+}
    // an array of objects for each quesiton and their corrisponding answers + boolean values
 var questions = [
     {question: "What is the purpose of a milkshake?",
@@ -34,13 +74,21 @@ var questions = [
 ];
 // create a variable with one parameter we will call question
 var newDiv;
+
 // function for setting and showing the next quesiton
 // create an index number to increase through each iteration
 var x = 0;
+
+var score = 0;
+
 function setQuestion() {
+    if (x === questions.length) {
+        endQuiz()
+    }
+    console.log(x)
     // create new div and append it to questionList
     newDiv = document.createElement('div');
-    newDiv.classList.add('btn');
+    newDiv.classList.add('newQuestion');
     // create an empty array to store value
     let questionName = [];
     // grab the index of the questions array and put it on the page
@@ -49,41 +97,48 @@ function setQuestion() {
     // use a loop to add the asnwers to the buttons
     for (let i = 0; i < questions[x].answers.length; i++) {
         const button = document.createElement('button');
-        console.log(button)
+        button.classList.add('btn')
         if (questions[x].answers[i].correct === true) {
             button.dataset.correct = true
         }
-        console.log(button.dataset.correct)
+        else {
+            button.dataset.correct = false
+        }
+        
         button.innerText = questions[x].answers[i].text;
         newDiv.appendChild(button)
         questionList.appendChild(newDiv)}
 
     x = x + 1;
-    console.log(x)
+    
     questionList.addEventListener("click", selectAnswer)
-
-        
 };
     
 function selectAnswer(event){
     newDiv.classList.add('hide');
-    event.target.matches('Button')
-    const selectedButton = event.target;
-    if (selectedButton === true) {
-        rightAnswer()
+    if (event.target.matches('Button')) {
+        const selectedButton = event.target;
+        console.log(selectedButton)
+        
+        if (selectedButton.dataset.correct === "true") {
+           rightAnswer()
+        }
+        else {
+           wrongAnswer()
+        }
     }
-    else {
-        wrongAnswer()
-    }
-    setQuestion();
+        setQuestion();
 };
 
 function rightAnswer() {
-    console.log("True")
+    console.log("This one logged True")
+    score = score + 1
 }
 function wrongAnswer() {
-    console.log("False")
+    console.log("This one logged False")
 }
+
+
 // function timeOut();
 
 // create a start button quiz that does 2 things
@@ -97,13 +152,13 @@ startQuiz.addEventListener('click', function() {
         
         if(secondsLeft === 0) {
         clearInterval(timerInterval);
-        //create a function for ending the quiz if timer runs out
+        endQuiz()
         }
     
     }, 1000);
     console.log(secondsLeft)
     // hide the initialContent
-    document.getElementById('initialContent').classList.add('hide');
+    frontContent.classList.add('hide');
     // now call the function to start the questions
     questionList.classList.remove('hide');
     setQuestion();
@@ -136,4 +191,4 @@ startQuiz.addEventListener('click', function() {
                 // increase the counter by 1 to cycle to the next question
                 // repeat everything done above
             // when the counter reaches the last number of the array
-                // dont show next question, show the final score page + submit for high scores. 
+                // dont show next question, show the final score page + submit for high scores.
